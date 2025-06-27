b7bda445391d10a03b09bebf28c019b6394e6cb8
crepaldihigor <crepaldihigor@gmail.com>
2025-06-27 13:22:34 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -11,7 +11,11 @@ const firebaseConfig = {
   appId: "1:590510592947:web:0f11eba4c424b6c404b1c6"
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
+
+// Inicializa a autenticação
 const auth = getAuth(app);
 
+// Exporta a autenticação para ser usada em outras partes do site
 export { auth };
