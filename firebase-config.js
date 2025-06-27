7b592f1209b4a9dced6d78d32c7f7a88ce745937
crepaldihigor <crepaldihigor@gmail.com>
2025-06-27 00:20:14 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,17 @@
-// Simulação de configuração do Firebase
+// firebase-config.js
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDTHwl-XZiJTR9EQdhfiDHnYWD8Pef44Gc",
+  authDomain: "upload-facil.firebaseapp.com",
+  projectId: "upload-facil",
+  storageBucket: "upload-facil.appspot.com",
+  messagingSenderId: "590510592947",
+  appId: "1:590510592947:web:0f11eba4c424b6c404b1c6"
 };
 
-export default firebaseConfig;
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth, onAuthStateChanged };
